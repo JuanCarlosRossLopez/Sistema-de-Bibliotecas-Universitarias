427807d7deb51e86b5c9c064c9afbc86a065c0a7
Cana23 <ethanbarea@gmail.com>
2024-11-09 23:30:13 -0500

    arreglando unos errores con git stash (es muy confuso)

--- frontend_SBU/src/App.jsx
+++ frontend_SBU/src/App.jsx
@@ -1,9 +1,9 @@
-import AppRouter from "./router/appRouter";
+import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
     <div className="App">
-      <AppRouter />
+      <AppRoutes />
     </div>
   );
 }
