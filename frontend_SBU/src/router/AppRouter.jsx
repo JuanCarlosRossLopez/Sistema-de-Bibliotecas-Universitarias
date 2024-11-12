25752fe65a60120a346a3f15a47aa420660dcbd4
JEKO25P <josiaskumul@gmail.com>
2024-11-11 19:47:24 -0500

    singup en proceso

--- frontend_SBU/src/router/AppRouter.jsx
+++ frontend_SBU/src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Visitor/Home';
+import Singup from '../pages/Visitor/Signup';
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/singup" element={<Singup />} />
       </Routes>
     </Router>
   );
