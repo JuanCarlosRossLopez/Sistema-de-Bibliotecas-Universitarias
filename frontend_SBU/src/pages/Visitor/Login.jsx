1bf1e0e33749931d44829d47df22f311672ec942
NierRep24 <danielsanchezmartinez064@gmail.com>
2024-11-13 23:19:55 -0500

    Ajustes Login y Signup 2

--- frontend_SBU/src/pages/Visitor/Login.jsx
+++ frontend_SBU/src/pages/Visitor/Login.jsx
@@ -13,22 +13,22 @@ function Login() {
 
     console.log(watch("example")) // watch input value by passing the name of it
     return (
-        <div className="h-screen w-full relative">
+        <div className="min-h-screen w-full relative flex flex-col items-center justify-center ">
             <div>
                 <img
                     src="/img/fondoLoginAndRegistrer.png"
                     alt="imagen"
                     className="h-full w-full object-cover absolute inset-0 -z-10" />
-                <div className="flex flex-col items-center justify-center h-full text-center text-white px-4 gap-10 md:gap-14 lg:gap-20">
-                    <p className="text-5xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-7xl font-extrabold leading-tight md:mt-20 sm:mt-20">
+                <div className="flex h-full text-center text-white px-4 gap-10 md:gap-14 lg:gap-20">
+                    <p className="text-5xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-7xl font-extrabold leading-tight md:mb-10 mt-[-200px] md:mt-1 lg:mt-1">
                         Bienvenido de vuelta
                     </p>
 
                 </div>
             </div>
 
-            <div className="flex items-center justify-center mt-5  sm:py-10">
-                <div className="bg-[#e9d2c4] border-[4px] border-yellow-900 rounded-2xl hover:border-yellow-600 transition-all duration-200 w-full max-w-xl mx-4 sm:mx-6 lg:mx-10">
+            <div className=" flex ">
+                <div className="bg-[#e9d2c4] border-[4px] border-yellow-900 rounded-2xl transition-all duration-200 w-full  max-w-xl mx-4 sm:mx-6 lg:mx-10">
                     <form
                         className="mx-4 sm:mx-8 md:mx-10 flex items-center space-y-4 py-8 sm:py-10 md:py-16 px-4 sm:px-10 md:px-20 font-semibold text-gray-500 flex-col"
                         onSubmit={handleSubmit(onSubmit)}
@@ -36,21 +36,21 @@ function Login() {
                         <h1 className="text-black text-4xl">Inicio de Sesión</h1>
 
                         <input
-                            className="w-full p-2 placeholder-white text-black bg-[#d2a89f] rounded-md border border-gray-700 focus:border-yellow-700 hover:border-yellow-500 transition-all duration-200"
+                            className="w-full p-2 placeholder-white text-black bg-[#d2a89f] rounded-md border border-gray-700 focus:border-yellow-700 hover:border-[#f0a779] transition-all duration-200"
                             placeholder="Correo"
                             {...register("example")}
                         />
 
                         <input
-                            className="w-full p-2 placeholder-white bg-[#d2a89f] rounded-md border border-gray-700 focus:border-yellow-700 hover:border-yellow-500 transition-all duration-200"
+                            className="w-full p-2 placeholder-white bg-[#d2a89f] rounded-md border border-gray-700 focus:border-white-700 hover:border-[#f0a779] transition-all duration-200"
                             placeholder="Contraseña"
                             {...register("exampleRequired", { required: true })}
                         />
 
-                        {errors.exampleRequired && <span>This field is required</span>}
+                        {errors.exampleRequired && <span className="">This field is required</span>}
 
                         <input
-                            className="w-full sm:w-44 p-2 bg-gray-50 rounded-full font-bold text-gray-900 border-[4px] border-gray-700 hover:border-yellow-500 transition-all duration-200"
+                            className="w-full sm:w-44 p-2 bg-gray-50 rounded-full font-bold text-gray-900 border-[4px] border-gray-700 hover:border-[#4a180dc4] transition-all duration-200"
                             type="submit"
                         />
                         <p className="text-center">
--- frontend_SBU/src/pages/Visitor/Signup.jsx
+++ frontend_SBU/src/pages/Visitor/Signup.jsx
@@ -14,7 +14,7 @@ function Signup() {
 
   console.log(watch("example")) // watch input value by passing the name of it
   return (
-    <div className="h-screen w-full relative ">
+    <div className="min-h-screen w-full relative flex flex-col items-center justify-center ">
 
       <div>
         <img
@@ -22,15 +22,15 @@ function Signup() {
           alt="imagen"
           className="h-full w-full object-cover absolute inset-0 -z-10" />
         <div className="flex flex-col items-center justify-center h-full text-center text-white px-4 gap-10 md:gap-14 lg:gap-20">
-          <p className="text-5xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-7xl font-extrabold leading-tight md:mt-20 sm:mt-20">
+          <p className="text-5xl sm:text-5xl md:text-6xl lg:text-5xl xl:text-7xl font-extrabold leading-tight md:mb-10 mt-[-200px] md:mt-1 lg:mt-1">
             Bienvenido
           </p>
 
         </div>
       </div>
 
-      <div className="flex items-center justify-center mt-5  sm:py-10">
-        <div className="bg-[#C19B93] border-[4px] border-yellow-900 rounded-2xl hover:border-yellow-600 transition-all duration-200 w-full max-w-xl mx-4 sm:mx-6 lg:mx-10">
+      <div className=" flex ">
+        <div className="bg-[#C19B93] border-[4px] border-yellow-900 rounded-2xl transition-all duration-200 w-full max-w-xl mx-4 sm:mx-6 lg:mx-10">
           <form
             className="mx-4 sm:mx-8 md:mx-10 flex items-center space-y-4 py-8 sm:py-10 md:py-16 px-4 sm:px-10 md:px-20 font-semibold text-gray-500 flex-col"
             onSubmit={handleSubmit(onSubmit)}
@@ -42,9 +42,10 @@ function Signup() {
               placeholder="Correo"
               {...register("example")}
             />
+            {errors.exampleRequired && <span>This field is required</span>}
 
             <input
-              className="w-full p-2 placeholder-white bg-[#e9d2c4] rounded-md border border-gray-700 focus:border-yellow-700 hover:border-yellow-500 transition-all duration-200"
+              className="w-full p-2 placeholder-white bg-[#e9d2c4] rounded-md border border-gray-700 focus:border-white-700 hover:border-[#f0a779] transition-all duration-200"
               placeholder="Contraseña"
               {...register("exampleRequired", { required: true })}
             />
@@ -52,11 +53,11 @@ function Signup() {
             {errors.exampleRequired && <span>This field is required</span>}
 
             <input
-              className="w-full sm:w-44 p-2 bg-gray-50 rounded-full font-bold text-gray-900 border-[4px] border-gray-700 hover:border-yellow-500 transition-all duration-200"
+              className="w-full sm:w-44 p-2 bg-gray-50 rounded-full font-bold text-gray-900 border-[4px] border-gray-700 hover:border-[#4a180dc4] transition-all duration-200"
               type="submit"
             />
-            <p className="text-center">
-              ¿Ya tienes una cuenta?
+            <p className=" text-center">
+              ¿Ya tienes una cuenta?<br />
               <a
                 className="font-semibold text-black hover:text-yellow-500 transition-all duration-200"
                 href="/Signin"
