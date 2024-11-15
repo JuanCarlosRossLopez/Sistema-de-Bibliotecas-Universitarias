3e2d15a4aa2a3f625e69a281d9e28c06fbd11495
JuanRossLopez <rosslopezjuancarlos18@gmail.com>
2024-11-14 19:42:26 -0500

    correción de userRepository.js

--- backend/src/repositories/userRepository.js
+++ backend/src/repositories/userRepository.js
@@ -1,1 +1,62 @@
-j
+const User = require('../models/User');
+
+const findUsers = async (body) => {
+    try {
+        return await User.findAll(body);
+    } catch (error) {
+        console.error("Error al emcontrar los usuarios", error)
+        throw error;
+    }
+};
+
+const findUserById = async (id) => {
+    try {
+        return await User.findByPk(id)
+    } catch (error) {
+        console.error("Error al encontrar el usuario")
+        throw error
+    }
+};
+
+const createUser = async (body) => {
+    try {
+        return await User.create(body)
+    } catch (error) {
+        console.error("Error al crear el usuario")
+        throw error
+    }
+};
+
+const updateUser = async (body, id) => {
+    try {
+        return await User.update(body, {
+            where: {
+                id_user: id
+            }
+        });
+    } catch (error) {
+        console.error("Error al actualizar el usuario", error);
+        throw error
+    }
+};
+
+const deleteUser = async (id) => {
+    try {
+        return await User.destroy({
+            where: {
+                id_user: id
+            }
+        });
+    } catch (error) {
+        console.error("Error al eliminar el usuario", error);
+        throw error;
+    }
+};
+
+module.exports = {
+    findUsers,
+    findUserById,
+    createUser,
+    updateUser,
+    deleteUser
+}
