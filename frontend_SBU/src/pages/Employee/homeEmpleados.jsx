e92780cb600f35aa557ae3b28a4b485923ad7f12
NierRep24 <danielsanchezmartinez064@gmail.com>
2024-11-14 21:19:17 -0500

    cambios noseq fnwiuw

--- frontend_SBU/src/pages/Employee/homeEmpleados.jsx
+++ frontend_SBU/src/pages/Employee/homeEmpleados.jsx
@@ -7,55 +7,55 @@ function HomeEmpleados() {
     <div className="bg-[#FFEFE5] min-h-screen w-full">
       <div className="flex justify-end">
         <div className="bg-[#834841] w-full md:w-8/12 lg:w-9/12 xl:w-10/12  h-14 md:h-28 flex pt-5 pl-16 md:pl-8 md:pt-16 lg:pt-10 lg:pl-4  font-bold text-3xl md:text-4xl lg:text-6xl text-white">
-          Bienvenido sicma
+          Bienvenido sicmas
         </div>
       </div>
       <div className="fixed top-0">
         <Sidebar />
       </div>
       <div className="flex flex-col justify-center items-center h-full p-4">
-  <div className="flex flex-col gap-10 lg:gap-20">
-    <div className="flex flex-col md:flex-row gap-4 justify-between w-full">
-      <div className="bg-[#C19B93] rounded-md flex-1">
-        <h1 className="p-8 font-bold text-sm md:text-lg lg:text-xl text-center md:text-left">
-          Devoluciones pendientes: <b>0</b>
-        </h1>
-      </div>
-      <div className="bg-[#C19B93] rounded-md flex-1">
-        <h1 className="p-8 font-bold text-sm md:text-lg lg:text-xl text-center md:text-left">
-          Libros reservados: <b>0</b>
-        </h1>
-      </div>
-    </div>
+        <div className="flex flex-col gap-10 lg:gap-20">
+          <div className="flex flex-col md:flex-row gap-4 justify-between w-full">
+            <div className="bg-[#C19B93] rounded-md flex-1">
+              <h1 className="p-8 font-bold text-sm md:text-lg lg:text-xl text-center md:text-left">
+                Devoluciones pendientes: <b>0</b>
+              </h1>
+            </div>
+            <div className="bg-[#C19B93] rounded-md flex-1">
+              <h1 className="p-8 font-bold text-sm md:text-lg lg:text-xl text-center md:text-left">
+                Libros reservados: <b>0</b>
+              </h1>
+            </div>
+          </div>
 
-    <div className="flex flex-col md:flex-row gap-4 lg:gap-14 justify-center w-full">
-      <div
-        className="bg-light mx-auto md:mx-0 p-4 border border-spacing-2 flex-1"
-        style={{
-          maxWidth: "450px",
-          minHeight: "200px",
-        }}
-      >
-        <p className="m-2 text-center text-lg sm:text-xl md:text-2xl">
-          <b>Ejemplo #2:</b> Grafico de barras
-        </p>
-        <BarsChart />
+          <div className="flex flex-col md:flex-row gap-4 lg:gap-14 justify-center w-full">
+            <div
+              className="bg-light mx-auto md:mx-0 p-4 border border-spacing-2 flex-1"
+              style={{
+                maxWidth: "450px",
+                minHeight: "200px",
+              }}
+            >
+              <p className="m-2 text-center text-lg sm:text-xl md:text-2xl">
+                <b>Ejemplo #2:</b> Grafico de barras
+              </p>
+              <BarsChart />
+            </div>
+            <div
+              className="bg-light mx-auto md:mx-0 p-4 border border-spacing-2 flex-1"
+              style={{
+                maxWidth: "450px",
+                minHeight: "200px",
+              }}
+            >
+              <p className="m-2 text-center text-lg sm:text-xl md:text-2xl">
+                <b>Ejemplo #2:</b> Grafico de barras
+              </p>
+              <BarsChart />
+            </div>
+          </div>
+        </div>
       </div>
-      <div
-        className="bg-light mx-auto md:mx-0 p-4 border border-spacing-2 flex-1"
-        style={{
-          maxWidth: "450px",
-          minHeight: "200px",
-        }}
-      >
-        <p className="m-2 text-center text-lg sm:text-xl md:text-2xl">
-          <b>Ejemplo #2:</b> Grafico de barras
-        </p>
-        <BarsChart />
-      </div>
-    </div>
-  </div>
-</div>
 
     </div>
   );
--- frontend_SBU/src/router/AppRouter.jsx
+++ frontend_SBU/src/router/AppRouter.jsx
@@ -5,6 +5,7 @@ import Signup from '../pages/Visitor/Signup';
 import Myspace from '../pages/Visitor/Myspace';
 import HomeE from '../pages/Employee/homeEmpleados';
 import HomeEmpleados from '../pages/Employee/homeEmpleados';
+import HomeAdmin from '../pages/Admin/HomeAdmin';
 
 function AppRoutes() {
   return (
@@ -15,7 +16,7 @@ function AppRoutes() {
         <Route path="/signup" element={<Signup/>} />
         <Route path="/myspace" element={<Myspace />} />
         <Route path="/homee" element={<HomeEmpleados />} />
-
+        <Route path='/HomeA' element={<HomeAdmin/>}/>
       </Routes>
     </Router>
   );
