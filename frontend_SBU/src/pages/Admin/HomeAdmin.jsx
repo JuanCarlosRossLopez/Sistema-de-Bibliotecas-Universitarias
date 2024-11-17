b8fbd3e2e81c75295d1100de75ca4c864e5c61e6
NierRep24 <danielsanchezmartinez064@gmail.com>
2024-11-17 18:50:52 -0500

    cambios sidebar, tablas y graficos 2

--- frontend_SBU/src/pages/Admin/HomeAdmin.jsx
+++ frontend_SBU/src/pages/Admin/HomeAdmin.jsx
@@ -1,64 +1,54 @@
 import React from "react";
-import Sidebar from "../../components/Sidebar";
+import NavbarEJ from "../../components/navbarEj";
+import SidebarEJ from "../../components/sidebarEj";
 import BarsChart from "../../components/BarsChart";
 
-function HomeAdmin() {
+function HomeAdminEJ() {
     return (
-        <div className="bg-[#FFEFE5] min-h-screen w-full">
-            <div className="flex justify-end">
-                <div className="flex bg-[#834841] w-full md:w-8/12 lg:w-9/12 xl:w-10/12 h-14 md:h-28 pt-5 pl-16 md:pl-8 md:pt-16 lg:pt-10 lg:pl-4 font-bold md:text-4xl lg:text-6xl text-white ">
-                    <h1 className="text-center">Dashboard Administrador</h1>
-                </div>
+        <div className="bg-[#FFEFE5] h-full min-h-screen w-full">
+            <NavbarEJ />
+            <div className=" fixed top-0">
+            <SidebarEJ />
             </div>
-            <div className="fixed top-0">
-                <Sidebar />
-            </div>
-            <div className="flex flex-col justify-center items-center h-full p-4">
-                <div className="flex flex-col gap-10 lg:gap-20">
-                    <div className="flex flex-col md:flex-row gap-4 justify-between w-full">
-                        <div className="bg-[#C19B93] rounded-md flex-1">
-                            <h1 className="p-8 font-bold text-sm md:text-lg lg:text-xl text-center md:text-left">
-                                Administradores Registrados: <b>0</b>
+            <div className="flex flex-col h-full p-4">
+                <div className="flex flex-col gap-8 lg:gap-16">
+                    <div className="flex flex-col md:flex-row gap-4 pt-16 sm:pt-16 md:pt-20 lg:pt-20 xl:pt-20 justify-between w-full">
+                        <div className="bg-[#C19B93] rounded-md flex-1 p-4 md:p-6 lg:p-8">
+                            <h1 className="font-bold text-base md:text-lg lg:text-xl text-center md:text-left">
+                                Administradores registrados: <b>0</b>
                             </h1>
                         </div>
-                        <div className="bg-[#C19B93] rounded-md flex-1">
-                            <h1 className="p-8 font-bold text-sm md:text-lg lg:text-xl text-center md:text-left">
-                                Empleados Registrados. <b>0</b>
+                        <div className="bg-[#C19B93] rounded-md flex-1 p-4 md:p-6 lg:p-8">
+                            <h1 className="font-bold text-base md:text-lg lg:text-xl text-center md:text-left">
+                                Empleados registrados: <b>0</b>
                             </h1>
                         </div>
                     </div>
 
-                    <div className="flex relative flex-col md:flex-row gap-4 lg:gap-14 justify-center w-full">
+                    <div className="flex flex-col md:flex-row gap-4 lg:gap-20 items-center ml-auto justify-center w-full">
                         <div
-                            className="bg-light mx-auto md:mx-0 p-4 border border-spacing-2 flex-1"
-                            style={{
-                                maxWidth: "450px",
-                                minHeight: "200px",
-                            }}
+                            className="bg-light mx-auto md:mx-0 p-4 border border-spacing-2 flex-1 max-h-[240px] xl:max-h-[300px] max-w-[400px] md:max-w-[400px] lg:max-w-[500px] xl:max-w-[500px]"
+                            style={{ minHeight: "200px" }}
                         >
-                            <p className="m-2 text-center text-lg sm:text-xl md:text-2xl">
+                            <p className="m-2 text-center text-lg sm:text-xl md:text-2xl xl:text-2xl">
                                 <b>Usuarios Registrados Mensuales</b>
                             </p>
                             <BarsChart />
                         </div>
                         <div
-                            className="bg-light mx-auto md:mx-0 p-4 border border-spacing-2 flex-1"
-                            style={{
-                                maxWidth: "450px",
-                                minHeight: "200px",
-                            }}
+                            className="bg-light mx-auto md:mx-0 p-4 border border-spacing-2 flex-1 max-h-[240px] xl:max-h-[300px] max-w-[400px] md:max-w-[400px] lg:max-w-[500px] xl:max-w-[500px]"
+                            style={{ minHeight: "200px" }}
                         >
                             <p className="m-2 text-center text-lg sm:text-xl md:text-2xl">
-                                <b>Libros Vendidos Mensuales</b>
+                                <b>Libros Vendidos Mensualmente</b>
                             </p>
                             <BarsChart />
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
 
-export default HomeAdmin;
+export default HomeAdminEJ;
--- frontend_SBU/src/router/AppRouter.jsx
+++ frontend_SBU/src/router/AppRouter.jsx
@@ -5,7 +5,7 @@ import Myspace from '../pages/Visitor/Myspace';
 import HomeE from '../pages/Employee/homeEmpleados';
 import HomeEmpleados from '../pages/Employee/homeEmpleados';
 import TablaEmpleados from '../pages/Employee/TablaEmpleados';
-import HomeAdminEJ from '../pages/Admin/HomeAdminEJ';
+import HomeAdminEJ from '../pages/Admin/HomeAdmin';
 import HomeAdmin from '../pages/Admin/HomeAdmin';
 import TablaUsuarios from '../pages/Admin/TablaUsuarios';
 
