8f2bf5827b7772c9479701aa411bfa121e632f1f
JEKO25P <josiaskumul@gmail.com>
2024-11-18 00:02:16 -0500

    iniciando backend con mac

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,6 +3,7 @@ require('dotenv').config(); // Asegúrate de cargar las variables de entorno
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql'
 });
 
