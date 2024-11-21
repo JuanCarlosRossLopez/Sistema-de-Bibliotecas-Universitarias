74a1108083628f2db33ef6c1e1c38d3c4eb4cf34
cristianLR773 <cristianlopezrodriguez770@gmail.com>
2024-11-21 00:21:08 -0500

    Ahora lo del login

--- frontend_SBU/src/contexts/AuthContext.jsx
+++ frontend_SBU/src/contexts/AuthContext.jsx
@@ -6,7 +6,10 @@ const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(() => {
+        const storedUser = localStorage.getItem('user');
+        return storedUser ? JSON.parse(storedUser) : null;
+    });
     const navigate = useNavigate();
 
     useEffect(() => {
