ec3e6c6d59faa2c71fde5e60c052157bbb33e356
cristianLR773 <cristianlopezrodriguez770@gmail.com>
2024-11-26 23:26:53 -0500

    Api de user id

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -10,6 +10,7 @@ router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 router.get('/rentbyuser/:userId/books/:bookId', userController.isBookRentedByUser);
+router.get('/rentbyuser/:userId', userController.getBookRentByUserId);
 
 
 module.exports = router;
